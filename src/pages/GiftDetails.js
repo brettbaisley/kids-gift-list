0c6d2d81a1aeefb70a60c8ee668435d102bb487d
Brett Baisley <brett.baisley@gmail.com>
2021-07-22 10:20:06 -0400

    Add links to products on gift details page Fixes #29

--- src/pages/GiftDetails.js
+++ src/pages/GiftDetails.js
@@ -28,7 +28,11 @@ const GiftDetailsPage = () => {
             <p className="gift-brand gift-detail-brand">{gift.brand}</p>
             <p className="gift-title gift-detail-title">{gift.title}</p>
             <p className="gift-price gift-detail-price">${gift.price}</p>
-            <p className="gift-detail-description">{gift.description}</p>
+            <p className="gift-detail-description">
+                {gift.description}
+                <br /><br />
+                <a href={gift.url} alt="Company's Product Page" target="_blank" rel="noreferrer nofollow">Company Page</a>
+            </p>
             <img src={gift.img_url} alt={gift.title} className="gift-img gift-detail-img" />
             <div>
                 <Link to="/gifts" className="btn btn-primary">&lt; Back</Link>     
